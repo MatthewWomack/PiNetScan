b69d0317e7fd1ab53466c29d75a442861c2281e1
MatthewWomack <mowomack1@catamount.wcu.edu>
2025-12-17 15:57:29 -0500

    Switched to faster nmap script scans

--- SpiNet.py
+++ SpiNet.py
@@ -32,7 +32,7 @@ def scan_network():
 
 def check_vulnerabilities(host_ip):
     nm = nmap.PortScanner()
-    nm.scan(host_ip, arguments='-sV --script vuln') # Runs nmap's vulnerability scanning scripts
+    nm.scan(host_ip, arguments='-sV --script=default,vuln') # Runs nmap's vulnerability scanning scripts
     vulns=[]
     # Iterate through every protocol (usually just tcp)
     for proto in nm[host_ip].all_protocols():
